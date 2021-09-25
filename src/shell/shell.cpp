9e751dc1a9c774c10af5ffae2561ab1a90502df5
alira <ariellira.al@gmail.com>
2021-09-24 19:18:00 -0700

    added instances of different Parsers

--- src/shell/shell.cpp
+++ src/shell/shell.cpp
@@ -1,8 +1,11 @@
 #include "shell.hpp"
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "../parser/comment_parser.hpp"
+#include "../parser/connector_parser.hpp"
 #include "../parser/group_parser.hpp"
 
 #define MAX_INPUT_SIZE 256
@@ -12,20 +15,26 @@ Shell::Shell() {}
 Shell::~Shell() {}
 
 void Shell::run() {
-  char user_input[MAX_INPUT_SIZE] = "echo \"hello world\" # hi \"test\"";
+  char user_input[MAX_INPUT_SIZE] =
+      "echo \"[hello] || (world)\" && cd ./directory # hi \"test\"";
 
   // get user input
   // this->print_prompt();
   // std::cin.getline(user_input, MAX_INPUT_SIZE);
 
-  // parse input
+  // Parse comments '#' from user input
   CommentParser comment_parser(user_input);
-  comment_parser.print_unparsed();
   std::string line_with_no_comments = comment_parser.parse();
-  comment_parser.print_parsed();
+  // comment_parser.print_unparsed();
 
-  GroupParser g_parser(line_with_no_comments);
-  g_parser.parse();
+  // Parse [] () from user's input
+  GroupParser grouping_parser(line_with_no_comments);
+  std::vector<std::string> semiparsed_tokens = grouping_parser.parse();
+
+  // Parse ' ', ';', '|', and '&' from user's input
+  ConnectorParser connector_parser(semiparsed_tokens);
+  std::vector<std::string> parsed_tokens = connector_parser.parse();
+  // connector_parser.print_parsed();
 }
 
 void Shell::print_prompt() { std::cout << "$ "; }
